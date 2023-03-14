32793e3c416bc4fc325bee99a146dd437a649090
Lema Kefyalew <50364939+lemakefyalew@users.noreply.github.com>
2023-03-14 11:51:31 +0300

    f

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,59 @@
 #!/usr/bin/python3
 """
-A function that displays the
-stats from the standard input
+Script that reads stdin line by line and computes metrics:
+- Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status
+                code> <file size>
+- After every 10 lines and/or a keyboard interruption (CTRL + C), print these
+    statistics from the beginning:
+Example:
+    File size: 5213
+    200: 2
+    401: 1
+    403: 2
+    404: 1
+    405: 1
+    500: 3
 """
-import re
 import sys
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
-                403: 0, 404: 0, 405: 0, 500: 0}
-print_counter = 0
-size_summation = 0
+stcd = {"200": 0, "301": 0, "400": 0, "401": 0,
+        "403": 0, "404": 0, "405": 0, "500": 0}
+summ = 0
 
 
-def print_logs():
+def prn_stats():
     """
-    Prints status codes to the logs
+    Function that print stats about log
     """
-    print(f"File size: {size_summation}")
-    for k, v in sorted(status_codes.items()):
-        if v != 0:
-            print(f"{k}: {v}")
+    global summ
+
+    print('File size: {}'.format(summ))
+    stcdor = sorted(stcd.keys())
+    for each in stcdor:
+        if stcd[each] > 0:
+            print('{}: {}'.format(each, stcd[each]))
 
 
 if __name__ == "__main__":
+    cnt = 0
     try:
-        for line in sys.stdin:
-            std_line = line.replace("\n", "")
-            log_list = re.split('- | "|" | " " ', str(std_line))
+        """ Iter the standar input """
+        for data in sys.stdin:
             try:
-                codes = log_list[-1].split(" ")
-                if int(codes[0]) not in status_codes.keys():
-                    continue
-                status_codes[int(codes[0])] += 1
-                print_counter += 1
-                size_summation += int(codes[1])
-                if print_counter % 10 == 0:
-                    print_logs()
+                fact = data.split(' ')
+                """ If there is a status code """
+                if fact[-2] in stcd:
+                    stcd[fact[-2]] += 1
+                """ If there is a lenght """
+                summ += int(fact[-1])
             except:
                 pass
-        print_logs()
+            """ Printing control """
+            cnt += 1
+            if cnt == 10:
+                prn_stats()
+                cnt = 0
     except KeyboardInterrupt:
-        print_logs()
+        prn_stats()
+        raise
+    else:
+        prn_stats()
