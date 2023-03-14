fbe09b4831c2ef08a44dd4149e0316a49148139a
Lema Kefyalew <50364939+lemakefyalew@users.noreply.github.com>
2023-03-14 12:02:16 +0300

    try this

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,26 @@
 #!/usr/bin/node
-
 const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films';
+const FILMID = process.argv[2];
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
+function doRequest (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject(error);
+      }
+    });
   });
-};
+}
 
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+async function main (filmId) {
+  const res = await doRequest(`${url}/${filmId}`);
+  for (const character of res.characters) {
+    const resCharcter = await doRequest(character);
+    console.log(resCharcter.name);
   }
-);
+}
+
+main(FILMID);
